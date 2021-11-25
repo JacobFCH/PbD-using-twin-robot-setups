e4516666890add3bb9d82dad54253cf675e6c898
JacobFCH <jacobch@live.dk>
2021-11-25 11:05:34 +0100

    Moved coppelia stuff into folders now coppelia is started from the coppeliaSim folder

--- launchCoppelia.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo launching Coppeliasim
-cd /home/jacob/workspace/CoppeliaSim_Edu_V4_2_0_Ubuntu18_04
-echo ~/gits/super-duper-thesis/$1
-./coppeliaSim.sh ~/gits/super-duper-thesis/$1
