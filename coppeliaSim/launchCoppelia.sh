ab91b0236f20f64a59f014483b6f1de0860e602b
JacobFCH <jacobch@live.dk>
2021-11-26 20:12:17 +0100

    Setting up Coppelia on Desktop

--- coppeliaSim/launchCoppelia.sh
+++ coppeliaSim/launchCoppelia.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo launching Coppeliasim
-cd /home/jacob/workspace/CoppeliaSim_Edu_V4_2_0_Ubuntu18_04
+cd /home/jacob/workspace/CoppeliaSim_Edu_V4_2_0_Ubuntu20_04
 echo ~/gits/super-duper-thesis/coppeliaSim/$1
 ./coppeliaSim.sh ~/gits/super-duper-thesis/coppeliaSim/$1
