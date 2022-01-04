67a2e4ed3411a1fb210afca764f68d18b23b8406
JacobFCH <jacobch@live.dk>
2022-01-04 11:18:50 +0100

    working admittance controller positional part

--- admittance_controller_test.py
+++ admittance_controller_test.py
@@ -3,74 +3,62 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-# Integrator and integrad function, not sure if this is correct
-def integrand(x, a):
-    return x*a
-
-def integrator(mat, timestep):
-    ret = np.zeros((3))
-    for i in range(mat.shape[0]):
-        I = quad(integrand,0,timestep, args=(mat[i]))
-        ret[i] = I[0]
-
-    return ret 
-
 # Method for computing the compliant positon at each timestep
-def compute_Comliance(desired_position, external_force, uD_p, uK_p, M_p, D_p, K_p, timestep):
-    sum = np.asarray(external_force - uD_p - uK_p)
+def compute_Comliance(d_p, h_e, uD_p, uK_p, pdd_cd, pd_cd, p_cd, M_p, D_p, K_p, timestep):
+    sum = np.asarray(h_e - uD_p - uK_p)
     pdd_cd = np.matmul(sum,np.linalg.pinv(M_p))
 
-    pd_cd = integrator(pdd_cd,timestep)
+    pd_cd = pd_cd + (pdd_cd * timestep)
 
-    uD_p = pd_cd * D_p
+    p_cd = p_cd + (pd_cd * timestep)
 
-    p_cd = integrator(pd_cd,timestep)
-
-    uK_p = p_cd * K_p
-
-    p_c = desired_position + p_cd
+    p_c = d_p + p_cd
 
     uD_p = np.matmul(pd_cd , D_p)
     uK_p = np.matmul(p_cd, K_p)
 
-    return uD_p, uK_p, p_c
+    return pdd_cd, pd_cd, p_cd, uD_p, uK_p, p_c
 
 # Initial Estimates
-desired_position = np.array([1,1,1])
-external_force = np.array([0,0,0])  
+d_p = np.array([1,1,1])
+h_e = np.array([0,0,0])  
 
 uD_p = np.array([0,0,0])
 uK_p = np.array([0,0,0])
 
+pdd_cd = np.array([0,0,0])
+pd_cd = np.array([0,0,0])
+p_cd = np.array([0,0,0])
+
 M_p = np.array([[1,0,0],[0,1,0],[0,0,1]])
-D_p = np.array([[1,0,0],[0,1,0],[0,0,1]])
-K_p = np.array([[1,0,0],[0,1,0],[0,0,1]])
+D_p = np.array([[10,0,0],[0,10,0],[0,0,10]])
+K_p = np.array([[0,0,0],[0,0,0],[0,0,0]])
 
 timestep = 0
-hz = 0.02
+dt = 1/50
 
 p_cs = []
 forces = []
 
 # Main loop for testing runs for 5 seconds
-while timestep < 5:
+while timestep < 10:
     # Computing compliant position using a integrating from 0 to 1, not sure if this is correct
-    uD_p, uK_p, p_c = compute_Comliance(desired_position, external_force, uD_p, uK_p, M_p, D_p, K_p, 1)
-    time.sleep(hz)
+    pdd_cd, pd_cd, p_cd, uD_p, uK_p, p_c = compute_Comliance(d_p, h_e, uD_p, uK_p, pdd_cd, pd_cd, p_cd, M_p, D_p, K_p, dt)
+    time.sleep(dt)
 
     # Adding an external force a 1 second
-    if timestep > 1 and timestep < 1 + hz:
+    if timestep > 1 and timestep < 1 + dt:
         print("adding external force")
-        external_force = np.array([1,0,0])
+        h_e = np.array([1,0,0])
 
     # Removing the external force at 4 seconds
-    if timestep > 4 and timestep < 4 + hz:
+    if timestep > 4 and timestep < 4 + dt:
         print("no external force")
-        external_force = np.array([0,0,0]) 
+        h_e = np.array([0,0,0]) 
 
-    timestep += hz
+    timestep += dt
     p_cs.append(p_c)
-    forces.append(external_force)
+    forces.append(h_e)
 
 # Plotting The compliant postion and the external forces
 p_cs = np.asarray(p_cs)
