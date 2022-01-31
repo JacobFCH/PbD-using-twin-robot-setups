71836f0822dbc3fdc67183bb2dce680bd905de1a
JacobFCH <jacobch@live.dk>
2022-01-31 20:32:19 +0100

    ?v2

--- coppeliaSim/launchCoppelia.sh
+++ coppeliaSim/launchCoppelia.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo launching Coppeliasim
 cd /home/jacob/workspace/CoppeliaSim_Edu_V4_2_0_Ubuntu20_04
-echo test
 echo ~/gits/super-duper-thesis/coppeliaSim/$1
 ./coppeliaSim.sh ~/gits/super-duper-thesis/coppeliaSim/$1
