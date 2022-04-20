Fixed sign jumps in the demonstration and added the changes inigo suggested, still not quite working tho

pythonScripts/dmp_cartesian.py
@@ -150,16 +150,12 @@ class DMP():
         d_p = np.gradient(p, axis=0) / dt
         dd_p = np.gradient(d_p, axis=0) / dt
 
-        d_o = []
-        dd_o = []
+        d_o = np.empty([len(o),3])
+        dd_o = np.empty([len(o),3])
 
-        for i in range(len(o) - 1):
-            d_o.append(2 * np.log(o[i + 1] * o[i].conjugate()))
-        for i in range(len(d_o) - 1):
-            dd_o.append(2 * np.log(d_o[i + 1] * d_o[i].conjugate()))
-        d_o = np.asarray(d_o)
-        print(d_o[-1])
-        dd_o = np.asarray(dd_o)
+        for i in range(1, len(o) - 1):
+            d_o[i] = quaternion.as_vector_part(2 * np.log(o[i + 1] * o[i].conjugate()))
+        dd_o += d_o * dt
 
         # Integrate canonical system
         x = self.cs.rollout(ts, tau)
@@ -174,7 +170,7 @@ class DMP():
 
         # np.log(self.go - o[j].conjugate())
         def forcing_o(j):
-            return Do_inv.dot(quaternion.as_vector_part((tau**2 * dd_o[j]) - (self.alpha_o * (self.beta_o * 2 * (np.log(self.go * o[j].conjugate()))) - (tau * d_o[j]))))
+            return Do_inv.dot((tau**2 * dd_o[j]) - (self.alpha_o * (self.beta_o * 2 * quaternion.as_vector_part(np.log(self.go * o[j].conjugate()))) - (tau * d_o[j])))
 
         A = np.stack(features(xj) for xj in x)
         f_p = np.stack(forcing_p(j) for j in range(len(ts)))

pythonScripts/dmp_test.py
@@ -1,29 +1,41 @@
 from dmp_cartesian import DMP
 import quaternion
 import numpy as np
+from scipy.spatial.transform.rotation import Rotation as R
 
 if __name__ == '__main__':
     # Load a demonstration file containing robot positions.
     demo = np.loadtxt("demonstration.dat", delimiter=" ", skiprows=1)
 
-    N = 50 #Number of filters
+    N = 50  # Number of filters
     dmp = DMP(n_bfs=N, alpha=48.0)
 
     tau = 0.002 * len(demo)
     t = np.arange(0, tau, 0.002)
     demo_p = demo[:, 0:3]
 
+    # Fix demonstration, Axis angles switch signs
+    demo_axis = demo[:, 0:3]
+    for i in range(1, len(demo_axis) - 1):
+        if np.dot(demo_axis[i + 1],demo_axis[i]) < 0:
+            demo_axis[i + 1] = -1 * demo_axis[i + 1]
+
     # Convert demonstration orientation to quaternions
-    demo_o = quaternion.from_euler_angles(demo[:, 3:6])
+    demo_o = quaternion.from_rotation_vector(demo_axis)
+
+    # Ensure that signs don't flip in quaternion space
+    for i in range(1, len(demo_o) - 1):
+        if np.dot(quaternion.as_float_array(demo_o[i + 1]),quaternion.as_float_array(demo_o[i])) < 0:
+            demo_o[i + 1] = -1 * demo_o[i + 1]
 
     dmp.train(demo_p, demo_o, t, tau)
 
     tau /= 1
 
-    dmp_p, dmp_dp, dmp_ddp, dmp_o, dmp_do, dmp_ddo = dmp.rollout(t, tau)  # Generate an output trajectory from the trained DMP
+    dmp_p, dmp_dp, dmp_ddp, dmp_o, dmp_do, dmp_ddo = dmp.rollout(t, tau) # Generate an output trajectory from the trained DMP
 
-    #dmp.plot2DDMP_Position(demo_p, dmp_p, t , t)
-    dmp.plot2DDMP_Orientation(demo[:, 3:6], quaternion.as_euler_angles(dmp_o), t , t)
+    # dmp.plot2DDMP_Position(demo_p, dmp_p, t , t)
+    dmp.plot2DDMP_Orientation(demo_axis, quaternion.as_rotation_vector(dmp_o), t , t)
 
 
 